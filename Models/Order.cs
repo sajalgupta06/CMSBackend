1aacc35fd22b608f61042c85224cf48ddb39737b
Sajal Gupta <guptasajal6@gmail.com>
2023-08-24 11:48:05 +0530

    created model

---
+++ Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMSBackend.Models
+{
+    public class Order
+    {
+        [Key] public int Id { get; set; }
+
+        [ForeignKey("Products")]
+        public Product[] Products { get; set; }
+
+        [ForeignKey("User")]
+        public User User { get; set; }
+
+        public string orderStatus { get; set; }
+
+        [Required]
+        public string paymentMethod { get; set; }
+        public float total { get; set; }
+        public DateAndTime createdBy { get; set; }
+
+    }
+}
